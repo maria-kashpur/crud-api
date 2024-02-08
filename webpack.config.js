dbef614c2834e0034d342141f9ca873e943773e2
maria-kashpur <kashpur.maria@icloud.com>
2024-02-08 14:05:08 +0300

    feat: set up the project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from "path";
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index",
+  target: "node",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "index.js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [new ESLintPlugin({ extensions: "ts" })],
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/i,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
