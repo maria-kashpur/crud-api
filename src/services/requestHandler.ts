ecc260268f9d11416b530d39f19eb2c80b938e69
maria-kashpur <kashpur.maria@icloud.com>
2024-02-12 23:57:00 +0300

    feat: add message for server

--- src/services/requestHandler.ts
+++ src/services/requestHandler.ts
@@ -1,8 +1,20 @@
 import { endpoints, StatusCodes, Response } from "../types/types";
 import Users from "../controllers/usersController";
 import { IncomingMessage } from "http";
+import { PORT, isMulti } from "../data/constants";
+import cluster from "cluster";
 
 const requestHandler = (req: IncomingMessage, res: Response) => {
+  if (isMulti && cluster.isWorker) {
+    console.log(
+      `Request: ${req.method} ${req.url} - Worker on a port ${process.env.WORKER_PORT}. Pid: ${process.pid}`,
+    );
+  } else {
+    console.log(
+      `Request: ${req.method} ${req.url} - Server on port ${PORT}. Pid: ${process.pid}`,
+    );
+  }
+
   const { url } = req;
 
   const isUserEndpoint = url?.startsWith(endpoints.users);
